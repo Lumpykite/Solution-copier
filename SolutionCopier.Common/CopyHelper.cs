ad34b0832a6aaa6550df5d52bcdda16e18868d33
Dmytro Khmelnytskyi <dkhmelnytskyi@deloitte.com>
2022-05-02 16:37:36 +0300

    initial commit

---
+++ SolutionCopier.Common/CopyHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace SolutionCopier.Common;
+
+public class CopyHelper
+{
+    public static void CopyBySource(string source, IEnumerable<string>? filter = null)
+    {
+        var target = source + " - copy";
+        var files = new DirectoryInfo(source).EnumerateFiles();
+        filter ??= new List<string>() { "bin", "obj", "packages" };
+        IEnumerable<(string SourcePath, string TargetPath)> filteredFiles = files
+            .Where(x => filter.Any(f => !x.FullName.Contains(f)))
+            .Select(x => (x.FullName, x.FullName.Replace(source, target)));
+        foreach (var file in filteredFiles)
+        {
+            var dir = Path.GetDirectoryName(file.TargetPath);
+            if (dir is not null && !Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            File.Copy(file.SourcePath, file.TargetPath, true);
+            Console.WriteLine(file.TargetPath);
+        }
+    }
+}
---
+++ SolutionCopier.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SolutionCopier.Console;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main(params string[] args)
+    {
+        switch (args.Length)
+        {
+            case 1:
+                SolutionCopier.Common.CopyHelper.CopyBySource(args[0]);
+                break;
+        }
+
+
+        System.Console.ReadKey();
+    }
+}
